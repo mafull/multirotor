4ed2bc30b64d214d86574ed342ffa7e19ba78110
Max Fuller <max.fuller01@btinternet.com>
2018-08-05 22:51:28 +0100

    Update class templates

--- code/templates/Class.cpp
+++ code/templates/Class.cpp
@@ -1,1 +1,11 @@
 #include "NAME_AS_IS.hpp"
+
+NAME_AS_IS::NAME_AS_IS()
+{
+
+}
+
+NAME_AS_IS::~NAME_AS_IS()
+{
+
+}
--- code/templates/Class.hpp
+++ code/templates/Class.hpp
@@ -1,5 +1,16 @@
 #ifndef __NAME_CAP_UND_HPP
 #define __NAME_CAP_UND_HPP
 
+class NAME_AS_IS
+{
+    public:
+        NAME_AS_IS();
+        ~NAME_AS_IS();
+        
+    protected:
 
-#endif	// __NAME_CAP_UND_HPP
+    private:
+
+};
+
+#endif  // __NAME_CAP_UND_HPP
