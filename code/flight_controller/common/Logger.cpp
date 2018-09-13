ce5a8c5ba776de67375ce3fe937776664806c4d0
Max Fuller <max.fuller01@btinternet,com>
2018-09-13 16:33:54 +0100

    Change logger to use a packet instead of just a c string and begin implemetation of a Loggable class

--- code/flight_controller/common/Logger.cpp
+++ code/flight_controller/common/Logger.cpp
@@ -2,7 +2,7 @@
 
 Logger::Logger() :
     _thread(*this),
-    _queue(QUEUE_SIZE, QUEUE_ITEM_SIZE)
+    _queue(QUEUE_SIZE, sizeof(Log_Packet_t))
 {
 }
 
@@ -18,15 +18,29 @@ void Logger::start()
 
 void Logger::log(const std::string& message)
 {
-    if (message.length() <= 128u) // @todo: Change to constant
-    {
-        _queue.Enqueue(const_cast<void *>(static_cast<const void *>(message.c_str()))); // @todo: Sort this out
-    }
-    else // Message is too long
-    {
-        // @todo: Add in message splitting
-        _queue.Enqueue(const_cast<void *>(static_cast<const void *>("TOO LONG"))); // @todo: This too
-    }
+    // if (message.length() <= 128u) // @todo: Change to constant
+    // {
+    //     _queue.Enqueue(const_cast<void *>(static_cast<const void *>(message.c_str()))); // @todo: Sort this out
+    // }
+    // else // Message is too long
+    // {
+    //     // @todo: Add in message splitting
+    //     _queue.Enqueue(const_cast<void *>(static_cast<const void *>("TOO LONG"))); // @todo: This too
+    // }
+
+    // Create a
+    Log_Packet_t packet =
+        {
+            .sender = "UNKNOWN",
+            .severity = Log_Severity_Debug,
+            .message = const_cast<char *>(message.c_str()) // @todo: Is this the best way to do this?
+        };
+    log(packet);
+}
+
+void Logger::log(const Log_Packet_t& packet)
+{
+    _queue.Enqueue(&packet);
 }
 
 
@@ -37,22 +51,31 @@ void Logger::Logger_Thread::Run()
 
     write("Logger_Thread running\n");
 
-    // Character buffer in which to store the received message
-    // @todo: replace char[x] with a structure inc. message source
-    char receivedMessage[128] = ""; // @todo: Change size to constant
+    // Struct in which to store the received packet
+    Log_Packet_t receivedPacket = { 0 }; 
 
     for (;;)
     {
         // Attempt to remove an item from the queue
-        const bool success = _parent._queue.Dequeue((void *)&receivedMessage);
+        const bool success = _parent._queue.Dequeue((void *)&receivedPacket);
 
         if (success) // Successfully removed an item from the queue
         {
-            // Print the received message
-            // @todo: Add formatting
-            write(std::string(receivedMessage) + '\n');
+            // Created a formatted string from the packet
+            auto formattedString = createFormattedStringFromPacket(receivedPacket);
+
+            // Send the string over UART
+            write(formattedString);
         }
     }
+}
+
+const std::string Logger::Logger_Thread::createFormattedStringFromPacket(const Log_Packet_t& packet)
+{
+    return std::string(
+        packet.sender + ' ' + // @todo: Add width
+        std::to_string(packet.severity) + ' ' + // @todo: Add width
+        packet.message + '\n');
 }
 
 void Logger::Logger_Thread::write(const std::string& message)
--- code/flight_controller/common/Logger.hpp
+++ code/flight_controller/common/Logger.hpp
@@ -1,14 +1,32 @@
 #ifndef __LOGGER_HPP
 #define __LOGGER_HPP
 
+#define QUEUE_MESSAGE_LENGTH 128u
+#define QUEUE_SENDER_LENGTH 16u
 #define QUEUE_SIZE      20u
-#define QUEUE_ITEM_SIZE (sizeof(char) * 128u)
-//#define QUEUE_ITEM_SIZE sizeof(uint32_t)
+//#define QUEUE_ITEM_SIZE (sizeof(char) * QUEUE_MESSAGE_LENGTH)
 
 #include "peripherals/UART.hpp"
 
 #include "queue.hpp"
 #include "thread.hpp"
+
+using Log_Severity_t = 
+    enum Log_Severity_t : uint8_t
+    {
+        Log_Severity_Error = 0,
+        Log_Severity_Warning,
+        Log_Severity_Info,
+        Log_Severity_Debug
+    };
+
+using Log_Packet_t = 
+    struct Log_Packet_t
+    {
+        char sender[QUEUE_SENDER_LENGTH];
+        Log_Severity_t severity;
+        char message[QUEUE_MESSAGE_LENGTH];
+    };
 
 
 
@@ -27,6 +45,7 @@ class Logger
         void Run();
 
     private:
+        const std::string createFormattedStringFromPacket(const Log_Packet_t& packet);
         void write(const std::string& message);
         
         Logger& _parent;
@@ -53,6 +72,52 @@ private:
     Logger_Thread _thread;
 
     cpp_freertos::Queue _queue;
+};
+
+class Loggable
+{
+public:
+    Loggable(const Logger& logger, const std::string& sender) :
+        _logger(logger),
+        _sender(sender)
+    {
+
+    }
+
+private:
+    void log(Log_Severity_t severity, const std::string& message)
+    {
+        Log_Packet_t packet = 
+            {
+                .sender = _sender,
+                .severity = severity,
+                .message = const_cast<char *>(message.c_str())
+            };
+        _logger.log(packet);
+    }
+
+    void logError(const std::string& message)
+    {
+        log(Log_Severity_Error, message);
+    }
+
+    void logWarning(const std::string& message)
+    {
+        log(Log_Severity_Warning, message);
+    }
+
+    void logInfo(const std::string& message)
+    {
+        log(Log_Severity_Info, message);
+    }
+
+    void logDebug(const std::string& message)
+    {
+        log(Log_Severity_Debug, message);
+    }
+
+    Logger& logger;
+    const std::string _sender;
 };
 
 #endif  // __LOGGER_HPP
