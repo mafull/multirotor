f2255daeea6a62c014511e3f5f046c60ca586f2f
Max Fuller <max.fuller01@btinternet.com>
2018-10-14 21:47:27 +0100

    Add I2C files (no implementation yet)

---
+++ code/flight_controller/source/peripherals/I2C.c
@@ -0,0 +1,24 @@
+// --- Public header ---
+#include "peripherals/I2C.h"
+
+// --- Private header ---
+#define I2C_PRIVATE
+    #include "peripherals/I2C_private.h"
+#undef I2C_PRIVATE
+
+
+// --- Project includes ---
+
+// --- Library includes ---
+
+// --- Standard includes ---
+
+
+/******************************************************************************
+  Public Function Implementations
+ ******************************************************************************/
+
+
+/******************************************************************************
+  Private Function Implementations
+ ******************************************************************************/
