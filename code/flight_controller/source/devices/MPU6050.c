85a3236e7e6108a7a2c63fb3d621921147b54a89
Max Fuller <max.fuller01@btinternet.com>
2018-10-15 22:01:36 +0100

    Add template MPU6050 files

---
+++ code/flight_controller/source/devices/MPU6050.c
@@ -0,0 +1,24 @@
+// --- Public header ---
+#include "?/MPU6050.h"
+
+// --- Private header ---
+#define MPU6050_PRIVATE
+    #include "?/MPU6050_private.h"
+#undef MPU6050_PRIVATE
+
+
+// --- Project includes ---
+
+// --- Library includes ---
+
+// --- Standard includes ---
+
+
+/******************************************************************************
+  Public Function Implementations
+ ******************************************************************************/
+
+
+/******************************************************************************
+  Private Function Implementations
+ ******************************************************************************/
