6eb4d39577496aed95ee214521e8116547523e51
Max Fuller <max.fuller01@btinternet.com>
2018-10-28 18:39:19 +0000

    Add PwmOutput files

---
+++ code/flight_controller/source/peripherals/PwmOutput.c
@@ -0,0 +1,24 @@
+// --- Public header ---
+#include "peripherals/PwmOutput.h"
+
+// --- Private header ---
+#define PWM_OUTPUT_PRIVATE
+    #include "peripherals/PwmOutput_private.h"
+#undef PWM_OUTPUT_PRIVATE
+
+
+// --- Project includes ---
+
+// --- Library includes ---
+
+// --- Standard includes ---
+
+
+/******************************************************************************
+  Public Function Implementations
+ ******************************************************************************/
+
+
+/******************************************************************************
+  Private Function Implementations
+ ******************************************************************************/
---
+++ code/flight_controller/source/peripherals/PwmOutput_config.c
@@ -0,0 +1,12 @@
+// --- Public header ---
+#include "peripherals/PwmOutput.h"
+
+// --- Private header ---
+#define PWM_OUTPUT_PRIVATE
+    #include "peripherals/PwmOutput_private.h"
+#undef PWM_OUTPUT_PRIVATE
+
+
+/******************************************************************************
+  Private Data
+ ******************************************************************************/
