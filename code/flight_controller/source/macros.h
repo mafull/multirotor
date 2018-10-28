96843ccd68b79f9b8c32284b8b21a8a475d83b24
Max Fuller <max.fuller01@btinternet.com>
2018-10-28 12:40:35 +0000

    Initial quick implementation of ENSURE()/UNREACHABLE()

--- code/flight_controller/source/macros.h
+++ code/flight_controller/source/macros.h
@@ -1,9 +1,10 @@
 #ifndef __MACROS_H
 #define __MACROS_H
 
+#define INF_LOOP for (;;) {}
 
-#define ENSURE(expression)  // @todo: Implement
-#define UNREACHABLE()       // @todo: Implement
+#define ENSURE(exp)         if (!(exp)) { INF_LOOP } // @todo: Implement
+#define UNREACHABLE()       INF_LOOP // @todo: Implement
 
 
 #endif // __MACROS_H
