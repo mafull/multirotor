fcf17fb47bf39e3abf5c1f84176eb3f16f02a916
Max Fuller <max.fuller01@btinternet.com>
2018-10-28 17:49:10 +0000

    Add Timer files

---
+++ code/flight_controller/source/peripherals/Timer.c
@@ -0,0 +1,24 @@
+// --- Public header ---
+#include "peripherals/Timer.h"
+
+// --- Private header ---
+#define TIMER_PRIVATE
+    #include "peripherals/Timer_private.h"
+#undef TIMER_PRIVATE
+
+
+// --- Project includes ---
+
+// --- Library includes ---
+
+// --- Standard includes ---
+
+
+/******************************************************************************
+  Public Function Implementations
+ ******************************************************************************/
+
+
+/******************************************************************************
+  Private Function Implementations
+ ******************************************************************************/
---
+++ code/flight_controller/source/peripherals/Timer_config.c
@@ -0,0 +1,12 @@
+// --- Public header ---
+#include "peripherals/Timer.h"
+
+// --- Private header ---
+#define TIMER_PRIVATE
+    #include "peripherals/Timer_private.h"
+#undef TIMER_PRIVATE
+
+
+/******************************************************************************
+  Private Data
+ ******************************************************************************/
